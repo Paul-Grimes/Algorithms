b6d9caa2e3951117129fb37b34ce876db2231d35
Paul Grimes <paul.grimes@byprime.com>
2013-07-23 16:14:11 -0400

    Added initial project and simple variance

---
+++ Variance/Variance.cs
@@ -0,0 +1,24 @@
+﻿namespace Variance
+{
+    using System;
+
+    public class Variance
+    {
+        public static double simple_variance(double[] values)
+        {
+            double total = 0;
+            double totaledSquared = 0;
+            double counter = 0;
+
+
+            foreach (double value in values)
+            {
+                counter++;
+                total += value;
+                totaledSquared += value * value;
+            }
+
+            return (totaledSquared - ((total * total) / counter)) / (counter - 1);
+        }
+    }
+}
