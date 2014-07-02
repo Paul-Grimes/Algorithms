7b6a36d7355d682b5c1d3287d8cbb9dece98cfa7
Paul Grimes <paul.grimes@byprime.com>
2014-07-02 17:09:02 -0400

    Added comments describing the type of problem.

--- knapsack/Knapsack.cs
+++ knapsack/Knapsack.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace knapsack
 {
+    /*
+     * The knapsack problem is a np complete problem.
+     * 
+     * A NP Complete problem is a problem that is nondeterministic in polynomial time.
+     * Simply put there are several ways that a solution can solve a nondeterministic
+     * problem. The solution can perform in polynomial time or exponential time depending
+     * on the decisions it makes during execution.  The solution is also not garunteed
+     * to run with consistant peformance from one execution to another.
+     * 
+     * Generally speaking, algorithms built to solve problems are deterministic in
+     * polynomial time.  In other words, the solution follows a consistant measurable path
+     * each time it is executed.  This algorithm will break the general speaking mold.
+     * 
+     * The goal here is to provide a nondeterministic algorithm to solve the knapsack problem.
+     * 
+     * Now that we have comunicated the complixity of the knapsack problem, lets 
+     * define what a knapsack problem is, and the objective of our solution.
+     */
     public class Knapsack
     {
     }
